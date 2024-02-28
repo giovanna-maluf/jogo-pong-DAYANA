9eb2f517c8ddf137be52a204de96ed1734d5530e
giovanna-maluf <135022646+giovanna-maluf@users.noreply.github.com>
2024-02-28 12:16:21 -0300

    Add files via upload

---
+++ jogo.pong/script.js
@@ -0,0 +1,36 @@
+
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 30;
+
+let vxBolinha = 9;
+let vyBolinha = 9;
+
+
+function setup() {
+   createCanvas(800, 600);
+  
+  }
+  
+
+  function draw() {
+    background(0);
+
+    circle(xBolinha,yBolinha,diametro);
+  
+  xBolinha += vxBolinha; 
+   
+  //AS DUAS BARRAS SIGNIFICA OU
+
+   if(xBolinha > width || xBolinha < 0 ){
+
+      vxBolinha *= -1;
+  }
+
+  yBolinha+=vyBolinha
+
+ if(yBolinha > height || yBolinha < 0) {
+
+    vyBolinha *= -1;
+  }
+}
